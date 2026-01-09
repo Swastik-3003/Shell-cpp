3b0eb610c9b0c8b7f542736c87ef5595cb526a7d
Hak3n <swastik30032005@gmail.com>
2026-01-09 18:36:12 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+using namespace std;
+void print_v(vector<string> v){
+  for(auto it : v){
+    cout << it << " ";
+  }
+  cout << "\n";
+}
 
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
+  cout << unitbuf;
+  cerr << unitbuf;
+  string command;
   while(true){
-    std::cout << "$ ";
-    std::string command;
-    std::cin >> command;
-    if(command == "exit")break;
-    std::cout << command << ": command not found" << std::endl;
+    cout << "$ ";
+    getline(cin,command);
+    vector<string> array;
+    string temp;
+    int size = command.size();
+    for(int i=0; i<size; i++){
+      if(command[i] == ' ' && temp != ""){
+        array.push_back(temp);
+        temp = "";
+      }
+      else{
+        temp += command[i];
+      }
+    }
+    if(temp != "")array.push_back(temp);
+     
+    if(array[0] == "echo"){
+      array.erase(array.begin());
+      print_v(array);
+      continue;
+    }
+    else if(array[0] == "exit"){
+      break;
+    }
+    else{
+      cout << command << ": command not found\n";
+    }
   }
 }
