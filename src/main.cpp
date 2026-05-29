a0644218139f3079fd81869ca5acfd94450bf250
Hak3n <swastik30032005@gmail.com>
2026-05-29 19:15:10 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,128 @@
-#include <cstdlib>
-#include <bits/stdc++.h>
-#include <filesystem>
 #include <iostream>
-#include <string>
 #include <vector>
-using namespace std;
+#include <string>
+#include <sstream>
+#include <functional>
+#include <map>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <linux/limits.h>
 
-void print_v(vector<string> v){
-  for(auto it : v){
-    cout << it << " ";
-  }
-  cout << "\n";
+bool should_exit = false; 
+using builtin_func_ptr = std::function<void(std::vector<std::string>&)>;
+extern std::map<std::string,builtin_func_ptr> builtin_map;
+
+
+void builtin_exit(std::vector<std::string>& args){
+  should_exit=true;
 }
 
-void type_exec(vector<string>& array){
-  const char* paths = getenv("PATH");
-  //storing paths in an array
-  string temp = "";
-  vector<string> paths_array;
-  for(int i=0; paths[i]!='\0'; i++){
-    if(paths[i]==':'){
-      paths_array.push_back(temp);
-      temp = "";
-      continue;
-    }
-    temp+=paths[i];
+void builtin_echo(std::vector<std::string>& args){
+  args.erase(args.begin());
+  for(auto& it:args){
+    std::cout<<it<<" ";
   }
-  //checking for each command by adding it to the end of each path
-  for(auto arg : array){
-    if(arg == "type" || arg == "exit" || arg == "echo"){
-      cout << arg << " is a shell builtin\n";
-      break;
+  std::cout<<"\n";
+}
+
+void builtin_type(std::vector<std::string>& args){
+  args.erase(args.begin());
+
+  for(auto& it:args){
+    auto f=builtin_map.find(it);
+    if(f!=builtin_map.end()){
+      std::cout<<it<<" is a shell builtin\n";
     }
-    bool path_exists = false;
-    for(auto path : paths_array){
-      // cout << path+arg << endl;
-      if(filesystem::exists(path+"/"+arg)){
-        // cout << path+arg << endl;
-        path_exists = true;
-        filesystem::file_status s = filesystem::status(path+"/"+arg);
-        filesystem::perms p = s.permissions();
-        if((p & filesystem::perms::owner_exec) != filesystem::perms::none)
-           {
-            cout << arg << " is " << path+"/"+arg << "\n";
-            
-            break;
-           }
+    else{
+      auto env_path=std::getenv("PATH");
+      std::stringstream ss(env_path);
+      std::string path="";
+      bool cmd_exists=false;
+      while(getline(ss,path,':')){
+        std::string full_path= path+'/'+it;
+        if(access(full_path.c_str(),F_OK)==0 && 
+          access(full_path.c_str(),X_OK)==0){
+          cmd_exists=true;
+          std::cout<<it<<" is "<<path<<"\n";  
+          break;
+        }
       }
+      if(!cmd_exists)
+        std::cout<<it<<": Not found\n";
     }
-    if(!path_exists){
-      cout << arg << ": not found\n";
-    }
+  }
+}
+
+void builtin_pwd(std::vector<std::string>& args){
+  char dir[PATH_MAX];
+  if(getcwd(dir,sizeof(dir))!=NULL){
+    std::cout<<dir<<"\n";
+  }
+  else{
+    std::cerr<<"Error running getcwd()\n";
+  }
+}
+std::map<std::string,builtin_func_ptr> 
+  builtin_map= { 
+    {"exit",builtin_exit},
+    {"echo",builtin_echo},
+    {"type",builtin_type},
+    {"pwd",builtin_pwd}
+  };
+
+void loop(){
+  std::cout << "$ ";
+  std::string cmd;
+  std::getline(std::cin, cmd);
+  std::stringstream ss(cmd);
+  std::vector<std::string> args;
+
+  std::string arg;
+  while(ss>>arg){
+      args.push_back(arg);
+  }
+  if(args.empty()) {
+    return;
   }
   
+  auto f=builtin_map.find(args[0]);
+  if(f!=builtin_map.end()){
+    f->second(args);
+    return;
+  }
+  
+  pid_t p=fork();
+  int status;
+  if(p==-1){
+    std::cerr<<"Failure\n";
+    exit(1);
+  }
+  else if(p==0){
+    std::vector<const char*> args_c;
+    for(const std::string& it:args){
+      args_c.push_back(it.c_str());
+    }
+    args_c.push_back(nullptr);
+    if(execv(args_c[0], const_cast<char* const*>(args_c.data()))<0){
+      std::cerr<<"No file exists\n";
+      exit(EXIT_FAILURE);
+    }
+    exit(0);
+  }
+  else{
+    waitpid(p,&status,0);
+  }
 }
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
-  string command;
-  while(true){
-    cout << "$ ";
-    getline(cin,command);
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
-    //parsing input line into an array
-    vector<string> array;
-    string temp;
-    int size = command.size();
-    
-    for(int i=0; i<size; i++){
-      if(command[i] == ' ' && temp != ""){
-        array.push_back(temp);
-        temp = "";
-      }
-      else{
-        temp += command[i];
-      }
-    }
-    if(temp != "")array.push_back(temp);
-
-    //checking for valid commands and executing
-    if(array[0] == "echo"){
-      array.erase(array.begin());
-      print_v(array);
-      continue;
-    }
-    else if(array[0] == "type"){
-      array.erase(array.begin());
-      type_exec(array); 
-    }
-    else if(array[0] == "exit"){
-      break;
-    }
-    else{
-      cout << array[0] << ": command not found\n";
-    }
+  while(!should_exit){
+      loop();
   }
+
+  return 0;
 }
